5d1114a97bd6b0051513f1426e6653537c36c1d1
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2025-03-06 12:28:57 -0300

    Delete joalheriaJoiasJoiaV1/joalheriaJoiasJoiaV2/joalheriaJoiasJoiaV1 directory

--- joalheriaJoiasJoiaV1/joalheriaJoiasJoiaV2/joalheriaJoiasJoiaV1/target/classes/static/script.js
+++
@@ -1,9 +0,0 @@
-function formatInput(event) {
-    const input = event.target;
-    const value = input.value.replace(/-/g, ''); 
-    if (value.length > 5) {
-        input.value = value.slice(0, 5) + '-' + value.slice(5);
-    } else {
-        input.value = value;
-    }
-}
